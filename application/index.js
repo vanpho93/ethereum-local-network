45b8bab9b28e547aca27e1411ecd2e99e6ccb0ff
Van Pho Nguyen <vanpho02@gmail.com>
2019-04-14 00:22:36 +0700

    init

--- application/index.js
+++ application/index.js
@@ -2,6 +2,7 @@ const { web3 } = require('./ethereum-instance');
 
 async function start() {
     const { ROOT_PUBLIC_KEY } = process.env;
+    console.log(ROOT_PUBLIC_KEY);
     const rootAccountBalance = await web3.eth.getBalance(ROOT_PUBLIC_KEY);
     console.log({ rootAccountBalance });
     process.exit(0);
