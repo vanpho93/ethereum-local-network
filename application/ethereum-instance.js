6891bf41e20bef745aac5b3394b0dbc4155a74a5
Van Pho Nguyen <vanpho02@gmail.com>
2019-04-15 00:56:09 +0700

    update application code

--- application/ethereum-instance.js
+++ application/ethereum-instance.js
@@ -1,10 +1,8 @@
 const Web3 = require('web3');
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const WEB3_LOCAL_API = 'http://localhost:8545';
+const { ROOT_PRIVATE_KEY, WEB3_API } = process.env;
 
-const { ROOT_PRIVATE_KEY } = process.env;
-
-const provider = new HDWalletProvider(ROOT_PRIVATE_KEY, WEB3_LOCAL_API);
+const provider = new HDWalletProvider(ROOT_PRIVATE_KEY, WEB3_API);
 const web3 = new Web3(provider);
 
-module.exports = { web3, WEB3_LOCAL_API };
+module.exports = { web3, WEB3_API };
